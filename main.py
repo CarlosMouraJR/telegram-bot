5401b78c907bb6bd93fcb94e3fad5814191df3ad
Carlos Moura <m.informatica@yahoo.com.br>
2024-10-22 01:56:53 -0300

    fix: added handler in main.py

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
-import os
+import json
 import asyncio
-from dotenv import load_dotenv
 from telegram_service import TelegramService
 from coin_gecko_service import CoinGeckoService
-from binance_service import BinanceService
 
-load_dotenv()
 
-binance_source = os.getenv('BINANCE_SOURCE', True)
-interval = int(os.getenv('INTERVAL', 20))
+async def monitor_prices(service, telegram_service):
+    messages = []
 
-async def monitor_prices(service, telegram_service, interval=500):
-    while True:
-        price_btc = service.get_price("bitcoin", currency="usd")
-        price_usdt = service.get_price("tether", currency="brl")
+    price_btc = service.get_price("bitcoin", currency="usd")
+    price_usdt = service.get_price("tether", currency="brl")
 
-        if price_btc is not None:
-            alert_message_btc = f"Preço atual do BTC/USDT: ${price_btc:.2f}"
-            await telegram_service.send_alert(alert_message_btc)
+    if price_btc is not None:
+        messages.append(f"Preço atual do BTC/USDT: ${price_btc:.2f}")
 
-        if price_usdt is not None:
-            alert_message_usdt = f"Preço atual do USDT/BRL: R${price_usdt:.2f}"
-            await telegram_service.send_alert(alert_message_usdt)
+    if price_usdt is not None:
+        messages.append(f"Preço atual do USDT/BRL: R${price_usdt:.2f}")
 
-        await asyncio.sleep(interval)
-        await telegram_service.delete_all_messages()
-        await asyncio.sleep(interval)
+    return messages
 
 async def main():
     telegram_service = TelegramService()
     service = CoinGeckoService()
 
-    await monitor_prices(service, telegram_service, interval=interval)
+    messages = await monitor_prices(service, telegram_service)
+    for message in messages:
+        await telegram_service.send_alert(message)
 
-# Adiciona a execução assíncrona correta
-if __name__ == "__main__":
-    asyncio.run(main())
+def handler(event, context):
+    try:
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'message': 'Monitoring prices started'})
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps({'error': str(e)})
+        }
